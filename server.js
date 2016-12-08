9fc0fc10dc790da63e35371fe59c87f6f24e3642
Benjamin White <benjamin.e.white1@gmail.com>
2016-12-08 16:38:03 -0500

    return object

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.put('/posts/:id', (req, res) => {
   });
 
   BlogPost
-    .findByIdAndUpdate(req.params.id, {$set: updated})
+    .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
     .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
     .catch(err => res.status(500).json({message: 'Something went wrong'}));
 });
