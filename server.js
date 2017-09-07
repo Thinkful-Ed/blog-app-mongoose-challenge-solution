ea3c52f25ae931b7e619482152e058a6660848de
Benjamin E White <benjamin.e.white1@gmail.com>
2017-09-07 16:23:05 -0400

    Send 204

--- server.js
+++ server.js
@@ -95,8 +95,7 @@ app.put('/posts/:id', (req, res) => {
 
   BlogPost
     .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
-    .exec()
-    .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
+    .then(updatedPost => res.status(204).end())
     .catch(err => res.status(500).json({message: 'Something went wrong'}));
 });
 
