08f25fefa601062f5bc7df14006dc252bd8d7bc6
Chris Klanac <cklanac@users.noreply.github.com>
2017-11-28 14:17:57 -0600

    fix: add `useMongoClient` to fix deprecaton warning

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ let server;
 // this function connects to our database, then starts the server
 function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
+    mongoose.connect(databaseUrl, { useMongoClient: true }, err => {
       if (err) {
         return reject(err);
       }
