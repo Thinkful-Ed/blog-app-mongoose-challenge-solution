780293a046c639d2ef07242e0d04964ffeb1ad4c
Chris Tsongas <chris.tsongas@gmail.com>
2018-01-23 18:37:05 +0000

    remove unnecessary flag

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ let server;
 // this function connects to our database, then starts the server
 function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, { useMongoClient: true }, err => {
+    mongoose.connect(databaseUrl, err => {
       if (err) {
         return reject(err);
       }
