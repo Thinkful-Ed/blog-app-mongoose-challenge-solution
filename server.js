5b61ef0684754232483440ec9d9cbfe647efba3b
Chris Tsongas <chris.tsongas@gmail.com>
2018-01-24 17:55:44 +0000

    remove parameter no longer needed after mongoose version bump

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ let server;
 // this function connects to our database, then starts the server
 function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, { useMongoClient: true }, err => {
+    mongoose.connect(databaseUrl, err => {
       if (err) {
         return reject(err);
       }
